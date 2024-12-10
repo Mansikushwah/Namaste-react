3519d640520a2c576f06f7b154e3fa9b5119ad82
Mansi <mansikushwah12345@gmail.com>
2024-12-10 16:25:40 +0530

    lecture1 completed

---
+++ lecture1/app.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{},"heading");
+const heading2=React.createElement("h2",{},"heading2");
+const container=React.createElement("div",{},[heading,heading2]);
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
